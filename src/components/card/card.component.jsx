1d231b58ec943f7dd8be5e4a6ff5afa8bee396a0
josh-soutar <josh.soutar@servicenow.com>
2021-07-01 10:24:34 +1000

    adding files for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./card.component.styles.css";
+
+export const Card = (props) => (
+  <div className="card-container">
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set3&size=250x250`}></img>
+    <h2>{props.monster.name}</h2>
+    <p>{props.monster.email}</p>
+  </div>
+);
